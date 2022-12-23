bed6d4dbe479209a49c4bfdea5b70d1aad6116d3
antonbashlovka <anton.bashlovka@gmail.com>
2022-12-23 13:31:55 +0500

    done task 1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+
+
+void printArray(int[] arr){
+    string res = "[";
+    foreach (double value in arr ){
+        res = res + Convert.ToString(value) + ", ";
+    }
+    res = res.TrimEnd(new Char[] {',', ' '}) + "]";
+
+    Console.WriteLine(res);
+}
+
+int countEvenNums(int[] arr)
+{
+    int count = 0;
+    foreach(int val in arr)
+    {
+        if (val % 2 == 0){ 
+            count++;
+        }
+    }
+    return count;
+}
+int[] fillArray(int[] arr){
+
+    Random rand = new Random();
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(100, 999);
+    }
+
+    return arr;
+}
+
+
+int[] array = new int[10];
+
+array = fillArray(array);
+
+printArray(array);
+
+Console.WriteLine(countEvenNums(array)); 
